5b0dd84752c45d0fe25fdf9a1360f15d5556e154
Dmitry <dmi6059@yandex.ru>
2022-10-27 08:59:45 +0300

    Обновил список зависимостей

--- common_libraries.py
+++ common_libraries.py
@@ -1,10 +1,10 @@
+import random
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
-#warnings.filterwarnings('ignore')
-sns.set_style('darkgrid')
 import scipy
 from scipy.io.wavfile import write
 from scipy.fft import fft, fftfreq
@@ -15,4 +15,9 @@ from sklearn.metrics import classification_report
 # Матрица ошибок классификатора
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import gdown
-
+from scipy.fft import fft, fftfreq
+from scipy.fft import rfft, rfftfreq
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
+#warnings.filterwarnings('ignore')
+sns.set_style('darkgrid')
+import random
--- nn_libraries.py
+++ nn_libraries.py
@@ -1,10 +1,7 @@
-import warnings
-warnings.filterwarnings('ignore')
-
 # Классы-конструкторы моделей нейронных сетей
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential, Model,  Sequential, load_model
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import concatenate, Input, Dense, Dropout, BatchNormalization, Flatten, Conv1D, Conv2D, LSTM, GlobalMaxPooling1D, MaxPooling1D, RepeatVector
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import concatenate, Input, multiply, Dense, Dropout, BatchNormalization, Flatten, Conv1D, Conv2D, LSTM, GlobalMaxPooling1D, MaxPooling1D, MaxPooling2D, RepeatVector, SpatialDropout1D, SimpleRNN, GRU, Bidirectional
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Adamax, Nadam, Ftrl
 from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from tensorflow.keras.utils import plot_model
