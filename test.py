1a6b9f53aad764773e27f765decac3daa47f8426
Dmitry <dmi6059@yandex.ru>
2022-12-04 20:42:07 +0300

    Реализована статистика моделей

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-from model_detect_new_signal_functions import prepare_data_1
+class A:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def summ(self):
+        return self.a + self.b
+
+class B(A):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+ex1 = A(10,5)
+print(ex1.summ())
+ex2 = B(10,20)
+print(ex2.summ())
